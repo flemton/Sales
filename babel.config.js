3d417f55961c51a9beea2ce482ad8748ab5579cf
Osman Alhassan <oalhassan847@gmail.com>
2024-02-26 11:12:02 +0000

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            /**
+             * Regular expression is used to match all files inside `./src` directory and map each `.src/folder/[..]` to `~folder/[..]` path
+             */
+            components: './src/components',
+            src: './src',
+          },
+          extensions: [
+            '.js',
+            '.jsx',
+            '.json',
+            '.tsx',
+            '.ts',
+          ],
+        },
+      ],
+    ],
   };
 };
