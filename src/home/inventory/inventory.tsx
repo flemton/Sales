3bf2cedda51bb694563cd97c0ad34c333afba53f
Osman Alhassan <oalhassan847@gmail.com>
2024-02-28 12:43:15 +0000

    Create inventory.tsx

---
+++ src/home/inventory/inventory.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import InventoryScreen from './inventory-screen/inventory-screen'
+
+const InventoryNavigator = () => {
+  const Stack = createNativeStackNavigator()
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Inventory" component={InventoryScreen} />
+    </Stack.Navigator>
+  )
+}
+
+export default InventoryNavigator
