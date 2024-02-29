b43a87d79097d4bdde375652a636e26939eb7e50
Osman Alhassan <oalhassan847@gmail.com>
2024-02-29 22:44:16 +0000

    Add employee component and function

---
+++ src/home/inventory/components/inventory-screen/inventory-screen-styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+import { fontScale } from "../../../../components/scale";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  search: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
+    padding: fontScale(10),
+  },
+  inventoryList: {
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    width: "100%",
+  },
+});
+
+export default styles;
