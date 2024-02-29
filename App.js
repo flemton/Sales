9f4bf3445f54a8b87910b59d9249b0bc9f5d2ac5
Osman Alhassan <oalhassan847@gmail.com>
2024-02-29 13:27:29 +0000

    Add check sign in check to show appropriate screen

--- App.js
+++ App.js
@@ -4,13 +4,24 @@ import { StyleSheet, Text, View } from "react-native";
 import Home from "./src/home/home";
 import { NavigationContainer } from "@react-navigation/native";
 import Auth from "./src/auth/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [admin, setAdmin] = useState(false);
+  const auth = getAuth();
+  useEffect(() => {
+    console.log("Admin before: ", admin);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setAdmin(user.email);
+    });
+    console.log("Admin after: ", admin);
+    return () => unsubscribe();
+  }, [auth]);
+
   return (
     <View style={styles.container}>
-      <NavigationContainer>
-        <Auth />
-      </NavigationContainer>
+      <NavigationContainer>{admin ? <Home /> : <Auth />}</NavigationContainer>
     </View>
   );
 }
