52f5f291c61f488c385ea3bf520fe6eb6b7957a4
Osman Alhassan <oalhassan847@gmail.com>
2024-02-29 13:28:58 +0000

    Create employees-styles.ts

---
+++ src/home/employees/employees-styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { heightpoint } from "../../components/scale";
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginVertical: heightpoint(8),
+  },
+});
+
+export default styles;
