8698a5bb04c3e4b14e36cd3d400b4309b5e240ae
Osman Alhassan <oalhassan847@gmail.com>
2024-02-29 22:39:32 +0000

    Implement search to show results from inventory

--- src/components/search/search-styles.ts
+++ src/components/search/search-styles.ts
@@ -1,7 +1,35 @@
 import { StyleSheet } from "react-native";
+import { fontScale } from "../scale";
 
 const styles = StyleSheet.create({
   container: {
+    margin: fontScale(15),
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
+    width: "90%",
+  },
+  searchBar__unclicked: {
+    padding: fontScale(10),
+    flexDirection: "row",
+    width: "95%",
+    backgroundColor: "#d9dbda",
+    borderRadius: fontScale(15),
+    alignItems: "center",
+  },
+  searchBar__clicked: {
+    padding: fontScale(10),
+    flexDirection: "row",
+    width: "80%",
+    backgroundColor: "#d9dbda",
+    borderRadius: fontScale(15),
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  input: {
+    fontSize: fontScale(20),
+    marginLeft: fontScale(10),
+    width: "90%",
   },
 });
 
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,13 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import styles from './search-styles'
+import React from "react";
+import { TextInput, View, Keyboard, Button } from "react-native";
+import { Feather, Entypo } from "@expo/vector-icons";
+import styles from "./search-styles";
 
-const Search = () => {
+const Search = ({ clicked, searchPhrase, setSearchPhrase, setClicked }) => {
   return (
     <View style={styles.container}>
-      <Text>Search</Text>
+      <View
+        style={
+          clicked ? styles.searchBar__clicked : styles.searchBar__unclicked
+        }
+      >
+        {/* search Icon */}
+        <Feather
+          name="search"
+          size={20}
+          color="black"
+          style={{ marginLeft: 1 }}
+        />
+        {/* Input field */}
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          value={searchPhrase}
+          onChangeText={setSearchPhrase}
+          onFocus={() => {
+            setClicked(true);
+          }}
+        />
+        {/* cross Icon, depending on whether the search bar is clicked or not */}
+        {clicked && (
+          <Entypo
+            name="cross"
+            size={20}
+            color="black"
+            style={{ padding: 1 }}
+            onPress={() => {
+              setSearchPhrase("");
+            }}
+          />
+        )}
+      </View>
+      {/* cancel button, depending on whether the search bar is clicked or not */}
+      {clicked && (
+        <View>
+          <Button
+            title="Cancel"
+            onPress={() => {
+              Keyboard.dismiss();
+              setClicked(false);
+            }}
+          ></Button>
+        </View>
+      )}
     </View>
-  )
-}
-
-export default Search
+  );
+};
+export default Search;
