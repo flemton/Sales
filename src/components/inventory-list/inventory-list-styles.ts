72f7e7437e487dd3740b1c97fa27c0e170f517cb
Osman Alhassan <oalhassan847@gmail.com>
2024-02-29 22:38:34 +0000

    Update inventory list to show items in inventory

--- src/components/inventory-list/inventory-list-styles.ts
+++ src/components/inventory-list/inventory-list-styles.ts
@@ -1,8 +1,26 @@
 import { StyleSheet } from "react-native";
+import { fontScale } from "../scale";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: "85%",
+    width: "100%",
+  },
+  item: {
+    marginHorizontal: fontScale(30),
+    marginVertical: fontScale(16),
+    borderBottomWidth: fontScale(2),
+    borderBottomColor: "lightgrey",
+  },
+  title: {
+    fontSize: fontScale(20),
+    fontWeight: "bold",
+    marginBottom: fontScale(5),
+    fontStyle: "italic",
+  },
+  details: {
+    fontSize: fontScale(15),
+    marginBottom: fontScale(5),
   },
 });
 
--- src/components/inventory-list/inventory-list.tsx
+++ src/components/inventory-list/inventory-list.tsx
@@ -1,13 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import styles from './inventory-list-styles'
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import styles from "./inventory-list-styles";
 
-const InventoryList = () => {
+const Item = ({ name, details }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{name}</Text>
+    <Text style={styles.details}>{details}</Text>
+  </View>
+);
+
+const InventoryList = ({ searchPhrase, setClicked, data }) => {
+  console.log("Prog: ", data);
+  const renderItem = ({ item }) => {
+    // when no input, show all
+    if (searchPhrase === "") {
+      return <Item name={item.name} details={item.details} />;
+    }
+    // filter of the name
+    if (
+      item.name
+        .toUpperCase()
+        .includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))
+    ) {
+      return <Item name={item.name} details={item.details} />;
+    }
+    // filter of the description
+    if (
+      item.details
+        .toUpperCase()
+        .includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))
+    ) {
+      return <Item name={item.name} details={item.details} />;
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>InventoryList</Text>
+    <View
+      onStartShouldSetResponder={() => setClicked(false)}
+      style={styles.container}
+    >
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default InventoryList
+export default InventoryList;
