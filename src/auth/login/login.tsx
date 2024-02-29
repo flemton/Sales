fe2b727db028b3185341e3c269cb873f3a62a85f
Osman Alhassan <oalhassan847@gmail.com>
2024-02-29 12:20:22 +0000

    Create login screen with rn form component

---
+++ src/auth/login/login.tsx
@@ -0,0 +1,70 @@
+import { View, Text, TouchableHighlight, Alert } from "react-native";
+import React, { useRef, useState } from "react";
+import styles from "./login-styles";
+import { Form, FormItem } from "react-native-form-component";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const Login = ({ navigation }) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const storeRef = useRef();
+  const [store, setStore] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const auth = getAuth();
+
+  return (
+    <View style={styles.container}>
+      <Form
+        onButtonPress={() => signInWithEmailAndPassword(auth, email, password)}
+        style={styles.form}
+        buttonStyle={styles.btn}
+      >
+        <FormItem
+          style={styles.input}
+          label="Store Name"
+          labelStyle={styles.label}
+          value={store}
+          isRequired
+          ref={storeRef}
+          onChangeText={(input) => setStore(input)}
+          placeholder="Enter the store name"
+        />
+        <FormItem
+          style={styles.input}
+          label="Email"
+          labelStyle={styles.label}
+          value={email}
+          isRequired
+          ref={emailRef}
+          onChangeText={(input) => setEmail(input)}
+          placeholder="Enter your email address"
+        />
+        <FormItem
+          label="Password"
+          labelStyle={styles.label}
+          style={styles.input}
+          isRequired
+          value={password}
+          onChangeText={(input) => setPassword(input)}
+          ref={passwordRef}
+          placeholder="Password"
+          secureTextEntry
+          placeholderTextColor="lightgrey"
+          showErrorIcon={false}
+          underneathText="Password cannot be empty"
+        />
+      </Form>
+      <View>
+        <TouchableHighlight
+          onPress={() => navigation.navigate("Register")}
+          style={styles.btn}
+        >
+          <Text style={styles.btnText}>Register</Text>
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
