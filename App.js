de12a335d2d5a60720053f3959f67b4b9c1c4a99
Osman Alhassan <oalhassan847@gmail.com>
2024-02-29 12:18:48 +0000

    Initialize app with firebase

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
+import "./src/auth/config/firebase";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./src/home/home";
 import { NavigationContainer } from "@react-navigation/native";
+import Auth from "./src/auth/auth";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <NavigationContainer>
-        <Home />
+        <Auth />
       </NavigationContainer>
     </View>
   );
---
+++ src/auth/config/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import env from "../../environment";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = env().firebase;
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
