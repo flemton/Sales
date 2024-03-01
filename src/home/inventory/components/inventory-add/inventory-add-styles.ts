85fcc80c0238078409bd3ce05de4b91f67196c59
Osman Alhassan <oalhassan847@gmail.com>
2024-03-01 02:42:05 +0000

    Add inventory add form and submit functionality

---
+++ src/home/inventory/components/inventory-add/inventory-add-styles.ts
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+import {
+  fontScale,
+  heightpoint,
+  widthpoint,
+} from "../../../../components/scale";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginVertical: heightpoint(2),
+    height: "80%",
+  },
+  form: {
+    textAlign: "right",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: widthpoint(100),
+    marginVertical: heightpoint(2),
+    height: "80%",
+  },
+  label: {
+    fontSize: fontScale(18),
+  },
+  input: {
+    height: heightpoint(5),
+    width: widthpoint(80),
+    marginHorizontal: 0,
+    marginVertical: heightpoint(2),
+    borderWidth: 1,
+  },
+  btn: {
+    backgroundColor: "blue",
+    padding: widthpoint(1),
+    borderRadius: widthpoint(2),
+    width: widthpoint(80),
+  },
+  btnText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
+
+export default styles;
