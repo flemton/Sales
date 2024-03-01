647a1ca7b750d4cb96b00b95215aa0e525b43ce7
Osman Alhassan <oalhassan847@gmail.com>
2024-03-01 02:42:52 +0000

    Add inventory list and search functiomality to landing

--- src/home/landing/components/landing-screen/landing-screen-styles.ts
+++ src/home/landing/components/landing-screen/landing-screen-styles.ts
@@ -1,19 +1,18 @@
 import { StyleSheet } from "react-native";
+import { fontScale } from "../../../../components/scale";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   search: {
-    flex: 1,
     flexDirection: "row",
     alignItems: "flex-start",
     justifyContent: "center",
     width: "100%",
-    padding: 10,
+    padding: fontScale(10),
   },
   inventoryList: {
-    flex: 1,
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "center",
--- src/home/landing/components/landing-screen/landing-screen.tsx
+++ src/home/landing/components/landing-screen/landing-screen.tsx
@@ -1,18 +1,50 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./landing-screen-styles";
 import Search from "../../../../components/search/search";
 import InventoryList from "../../../../components/inventory-list/inventory-list";
 import Cart from "../cart/cart";
+import { ref, getDatabase, child, get } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 const Landing = () => {
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [inventoryData, setInventoryData] = useState();
+  const auth = getAuth();
+  const db = ref(getDatabase());
+  useEffect(() => {
+    get(
+      child(db, "stores/" + auth.currentUser.email.split("@")[0] + "/inventory")
+    )
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          setInventoryData(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [db]);
+
   return (
     <View style={styles.container}>
       <View style={styles.search}>
-        <Search />
+        <Search
+          clicked={clicked}
+          searchPhrase={searchPhrase}
+          setSearchPhrase={setSearchPhrase}
+          setClicked={setClicked}
+        />
       </View>
       <View style={styles.inventoryList}>
-        <InventoryList />
+        <InventoryList
+          searchPhrase={searchPhrase}
+          setClicked={setClicked}
+          data={inventoryData ? Object.values(inventoryData) : []}
+        />
         <Cart />
       </View>
     </View>
