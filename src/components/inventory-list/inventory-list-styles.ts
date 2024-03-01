ab1b59e1c48a14af272cd3d6bdd28df11e363a71
Osman Alhassan <oalhassan847@gmail.com>
2024-03-01 02:36:47 +0000

    Add price to inventory item

--- src/components/inventory-list/inventory-list-styles.ts
+++ src/components/inventory-list/inventory-list-styles.ts
@@ -1,16 +1,20 @@
 import { StyleSheet } from "react-native";
-import { fontScale } from "../scale";
+import { fontScale, widthpoint } from "../scale";
 
 const styles = StyleSheet.create({
   container: {
-    height: "85%",
+    alignItems: "center",
+    height: "88%",
     width: "100%",
   },
   item: {
-    marginHorizontal: fontScale(30),
-    marginVertical: fontScale(16),
-    borderBottomWidth: fontScale(2),
-    borderBottomColor: "lightgrey",
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#000000",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    width: widthpoint(82),
   },
   title: {
     fontSize: fontScale(20),
@@ -21,6 +25,18 @@ const styles = StyleSheet.create({
   details: {
     fontSize: fontScale(15),
     marginBottom: fontScale(5),
+  },
+  priceContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  price: {
+    fontSize: fontScale(18),
+    fontWeight: "bold",
+    alignSelf: "flex-end",
   },
 });
 
--- src/components/inventory-list/inventory-list.tsx
+++ src/components/inventory-list/inventory-list.tsx
@@ -2,19 +2,27 @@ import { View, Text, FlatList } from "react-native";
 import React from "react";
 import styles from "./inventory-list-styles";
 
-const Item = ({ name, details }) => (
+const Item = ({ name, details, unitPrice }) => (
   <View style={styles.item}>
-    <Text style={styles.title}>{name}</Text>
+    <View style={styles.priceContainer}>
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.price}>GH₵: {unitPrice}</Text>
+    </View>
     <Text style={styles.details}>{details}</Text>
   </View>
 );
 
 const InventoryList = ({ searchPhrase, setClicked, data }) => {
-  console.log("Prog: ", data);
   const renderItem = ({ item }) => {
     // when no input, show all
     if (searchPhrase === "") {
-      return <Item name={item.name} details={item.details} />;
+      return (
+        <Item
+          name={item.name}
+          details={item.description}
+          unitPrice={item.unitPrice}
+        />
+      );
     }
     // filter of the name
     if (
@@ -22,15 +30,27 @@ const InventoryList = ({ searchPhrase, setClicked, data }) => {
         .toUpperCase()
         .includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))
     ) {
-      return <Item name={item.name} details={item.details} />;
+      return (
+        <Item
+          name={item.name}
+          details={item.description}
+          unitPrice={item.unitPrice}
+        />
+      );
     }
     // filter of the description
     if (
-      item.details
+      item.description
         .toUpperCase()
         .includes(searchPhrase.toUpperCase().trim().replace(/\s/g, ""))
     ) {
-      return <Item name={item.name} details={item.details} />;
+      return (
+        <Item
+          name={item.name}
+          details={item.description}
+          unitPrice={item.unitPrice}
+        />
+      );
     }
   };
 
