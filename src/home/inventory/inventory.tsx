917401e635b8b6021e20a5e8640a8ec70c91dbd6
Osman Alhassan <oalhassan847@gmail.com>
2024-03-01 02:40:56 +0000

    Add inventory add screen to inventory navigator

--- src/home/inventory/inventory.tsx
+++ src/home/inventory/inventory.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import InventoryScreen from "./components/inventory-screen/inventory-screen";
+import InventoryAdd from "./components/inventory-add/inventory-add";
 
 const InventoryNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -8,6 +9,7 @@ const InventoryNavigator = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Inventory" component={InventoryScreen} />
+      <Stack.Screen name="InventoryAdd" component={InventoryAdd} />
     </Stack.Navigator>
   );
 };
